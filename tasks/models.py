8951e99baf03c263456233986bc7f36dc440530c
Avram88 <avramscp@gmail.com>
2014-08-01 21:19:59 +0200

    Models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BaseModel(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    
+    user = models.ForeignKey(User)
+    
+    class Meta:
+        abstract = True
+    
+class WorkflowInst(BaseModel):
+    type = models.CharField(max_length=200)
+    
+    class Meta:
+        db_table = 'workflow_inst'
+
+class TaskInst(BaseModel):    
+    name = models.CharField(max_length=200)
+    ordinal = models.PositiveIntegerField()
+    deadline = models.DateTimeField()
+    description = models.TextField()
+    finished = models.BooleanField(default = False)    
+    
+    workflow = models.ForeignKey(WorkflowInst)
+        
+    class Meta:
+        db_table = 'task_inst'
